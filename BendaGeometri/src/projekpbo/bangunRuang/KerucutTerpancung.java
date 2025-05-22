2c5b4a6e61ac83e3e4971c38b087f42a9fceb30c
nikita74939 <itanikita20005@gmail.com>
2025-05-22 09:01:17 +0700

    up kerucut terpancung

--- BendaGeometri/src/projekpbo/bangunRuang/KerucutTerpancung.java
+++ BendaGeometri/src/projekpbo/bangunRuang/KerucutTerpancung.java
@@ -1,33 +1,40 @@
 package projekpbo.bangunRuang;
+import projekpbo.bangunDatar.Lingkaran;
 
 public class KerucutTerpancung extends Lingkaran {
 
     private double jarijariAtas;
-
-    private double jariJari;
-
+    private double jariJari; // jari-jari alas
     private double tinggi;
-
     private double volumeKerucutTerpancung;
-
     private double luasPermukaanKerucutTerpancung;
 
     public KerucutTerpancung(double tinggi, double jarijariAtas, double jariJari) {
+        super(jariJari);
+        this.tinggi = tinggi;
+        this.jarijariAtas = jarijariAtas;
+        this.jariJari = jariJari;
     }
 
     public double hitungVolume() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        volumeKerucutTerpancung = (1.0 / 3.0) * Math.PI * tinggi * 
+            (jariJari * jariJari + jariJari * jarijariAtas + jarijariAtas * jarijariAtas);
+        return volumeKerucutTerpancung;
     }
 
     public double hitungLuasPermukaan() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        double s = Math.sqrt((jariJari - jarijariAtas) * (jariJari - jarijariAtas) + tinggi * tinggi);
+        luasPermukaanKerucutTerpancung = Math.PI * (jariJari + jarijariAtas) * s +
+                                          Math.PI * jariJari * jariJari +
+                                          Math.PI * jarijariAtas * jarijariAtas;
+        return luasPermukaanKerucutTerpancung;
     }
 
     public double getLuasPermukaanKerucutTerpancung() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return luasPermukaanKerucutTerpancung;
     }
 
     public double getVolumeKerucutTerpancung() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return volumeKerucutTerpancung;
     }
 }
