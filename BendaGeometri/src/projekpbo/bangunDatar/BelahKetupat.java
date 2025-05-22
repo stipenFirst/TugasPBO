1f267c601968d95d8e7ae5fc29f6d8cf798bd9a1
nikita74939 <itanikita20005@gmail.com>
2025-05-22 08:12:04 +0700

    update belahketupat

--- BendaGeometri/src/projekpbo/bangunDatar/BelahKetupat.java
+++ BendaGeometri/src/projekpbo/bangunDatar/BelahKetupat.java
@@ -1,33 +1,38 @@
 package projekpbo.bangunDatar;
+import projekpbo.Geometri2D;
 
 public class BelahKetupat extends Geometri2D {
 
     public double diagonal1;
-
     public double diagonal2;
-
     public double sisimiringBelahKetupat;
 
     private double luasBelahKetupat;
-
     private double kelilingBelahKetupat;
 
     public BelahKetupat(double diagonal1, double diagonal2) {
+        this.diagonal1 = diagonal1;
+        this.diagonal2 = diagonal2;
+
+        this.sisimiringBelahKetupat = Math.sqrt(Math.pow(diagonal1 / 2, 2) + Math.pow(diagonal2 / 2, 2));
+
+        this.luasBelahKetupat = hitungLuas();
+        this.kelilingBelahKetupat = hitungKeliling();
     }
 
     public double hitungLuas() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return 0.5 * diagonal1 * diagonal2;
     }
 
     public double getLuasBelahKetupat() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return luasBelahKetupat;
     }
 
     public double hitungKeliling() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return 4 * sisimiringBelahKetupat;
     }
 
     public double getKelilingBelahKetupat() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return kelilingBelahKetupat;
     }
 }
