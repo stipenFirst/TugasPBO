61eb095135948dc5b7200ebcb8fe1df15e776548
nikita74939 <itanikita20005@gmail.com>
2025-05-22 08:25:39 +0700

    update prismalayanglayang

--- BendaGeometri/src/projekpbo/bangunDatar/LayangLayang.java
+++ BendaGeometri/src/projekpbo/bangunDatar/LayangLayang.java
@@ -1,35 +1,42 @@
 package projekpbo.bangunDatar;
 
+import projekpbo.Geometri2D;
+
 public class LayangLayang extends Geometri2D {
 
     public double diagonal1;
-
     public double diagonal2;
-
     public double sisi1;
-
     public double sisi2;
 
     private double luasLayangLayang;
-
     private double kelilingLayangLayang;
 
     public LayangLayang(double diagonal1, double diagonal2, double sisi1, double sisi2) {
+        this.diagonal1 = diagonal1;
+        this.diagonal2 = diagonal2;
+        this.sisi1 = sisi1;
+        this.sisi2 = sisi2;
+
+        this.luasLayangLayang = hitungLuas();
+        this.kelilingLayangLayang = hitungKeliling();
     }
 
     public double hitungLuas() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        // Rumus luas layang-layang = ½ × diagonal1 × diagonal2
+        return 0.5 * diagonal1 * diagonal2;
     }
 
     public double getLuasLayangLayang() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return luasLayangLayang;
     }
 
     public double hitungKeliling() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        // Keliling = 2 × (sisi1 + sisi2)
+        return 2 * (sisi1 + sisi2);
     }
 
     public double getKelilingLayangLayang() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return kelilingLayangLayang;
     }
 }
--- BendaGeometri/src/projekpbo/bangunRuang/PrismaLayangLayang.java
+++ BendaGeometri/src/projekpbo/bangunRuang/PrismaLayangLayang.java
@@ -1,29 +1,31 @@
 package projekpbo.bangunRuang;
-
+import projekpbo.bangunDatar.LayangLayang;
 public class PrismaLayangLayang extends LayangLayang {
 
     private double tinggiPrismaLayangLayang;
-
     private double volumePrismaLayangLayang;
-
     private double luasPermukaanPrismaLayangLayang;
 
-    public PrismaLayangLayang(double diagonal1, double diagonal2, double sisi1, double sisi2, double tinggiPrismajajarGenjang) {
+    public PrismaLayangLayang(double diagonal1, double diagonal2, double sisi1, double sisi2, double tinggiPrismaLayangLayang) {
+        super(diagonal1, diagonal2, sisi1, sisi2);
+        this.tinggiPrismaLayangLayang = tinggiPrismaLayangLayang;
+        this.volumePrismaLayangLayang = hitungVolume();
+        this.luasPermukaanPrismaLayangLayang = hitungLuasPermukaan();
     }
 
     public double hitungVolume() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return getLuasLayangLayang() * tinggiPrismaLayangLayang;
     }
 
-    public double getVolumePrismaJajarGenjang() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public double getVolumePrismaLayangLayang() {
+        return volumePrismaLayangLayang;
     }
 
     public double hitungLuasPermukaan() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return (2 * getLuasLayangLayang()) + (getKelilingLayangLayang() * tinggiPrismaLayangLayang);
     }
 
     public double getLuasPermukaanPrismaLayangLayang() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return luasPermukaanPrismaLayangLayang;
     }
 }
